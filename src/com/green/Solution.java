8dada6bfa1b30d8687cbd2fec0d23ef6ba42668c
GreenTheSnail <soshkovladislav@gmail.com>
2021-02-06 20:32:52 +0100

    Two Sum

--- src/com/green/Solution.java
+++ src/com/green/Solution.java
@@ -4,13 +4,12 @@ public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] rt = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            for (int y = 1; y < nums.length; ) {
+            for (int y = 1 + i; y < nums.length; y++) {
                 if (nums[i] + nums[y] == target) {
                     rt[0] = i;
                     rt[1] = y;
-                    break;
+
                 }
-                y = y + i + 1;
             }
         }
         return rt;
