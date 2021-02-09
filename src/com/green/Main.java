ab2cb545907908e9487fde3f98479d5806c7d770
GreenTheSnail <soshkovladislav@gmail.com>
2021-02-09 21:30:48 +0100

    Palindrome Number

--- src/com/green/Main.java
+++ src/com/green/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String lul = "MCMXCIV";
-        int x =solution.romanToInt(lul);
+        int lul = 1000021;
+        boolean x =solution.isPalindrome(lul);
         System.out.println(x);
     }
     }
--- src/com/green/Solution.java
+++ src/com/green/Solution.java
@@ -1,29 +1,21 @@
 package com.green;
 
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class Solution {
-    public int romanToInt(String s) {
-        HashMap<String, Integer> letters = new HashMap();
-        int value = 0;
-        letters.put("I", 1);
-        letters.put("V", 5);
-        letters.put("X", 10);
-        letters.put("L", 50);
-        letters.put("C", 100);
-        letters.put("D", 500);
-        letters.put("M", 1000);
-        System.out.println(letters);
-        String[] romans = s.split("");
-        for (String roman : romans) {
-            value += letters.get(roman);
-        }
-        for (int i=1; i<romans.length; i++){
-            if(romans[i-1].equals("I") && romans[i].equals("V") || romans[i-1].equals("I") && romans[i].equals("X") || romans[i-1].equals("X") && romans[i].equals("L") || romans[i-1].equals("X") && romans[i].equals("C") || romans[i-1].equals("C") && romans[i].equals("D") || romans[i-1].equals("C") && romans[i].equals("M")){
-                value = value - (2 * letters.get(romans[i -1]));
-            }
-        }
-        return value;
+    public boolean isPalindrome(int x) {
+          boolean bol = false;
+          String[] nums = Integer.toString(x).split("");
+          if(nums.length == 1){
+              bol = true;
+          }else {
+              for (int i = 0; i < (nums.length / 2); i++) {
+                  if (nums[i].equals(nums[nums.length - i - 1])) {
+                      bol = true;
+                  } else {
+                      bol = false;
+                      break;
+                  }
+              }
+          }
+        return bol;
     }
 }
